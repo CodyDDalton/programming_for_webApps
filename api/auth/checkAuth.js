e07f258418e89de4c42b7eabc864e25be605abd9
unknown <cddalton@student.fullsail.edu>
2022-12-11 14:07:36 -0500

    JWT integrated

--- api/auth/checkAuth.js
+++ api/auth/checkAuth.js
@@ -7,6 +7,7 @@ try{
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.jwt);
     req.userData = decoded;
+    next();
 }
 catch(error){
     res.status(401).json({
